f14382af8432b44270cae645623755099dd5ceb6
DavidZarfati <zarda2001@gmail.com>
2026-01-22 15:16:22 +0100

    feat: correzione da post a film

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -6,7 +6,7 @@ function index(req, res) {
     let params = []
     connection.query(sql, params, (err, results) => {
         if (err) {
-            return res.status(500).json({ error: "Errore nel recupero dei post", details: err });
+            return res.status(500).json({ error: "Errore nel recupero dei film", details: err });
         }
         res.json({
             count: results.length,
@@ -23,7 +23,7 @@ function show(req, res) {
             descrizione: "L'id fornito non è valido."
         });
     }
-    // Recupera il post e i tag associati tramite JOIN
+    // Recupera il film e i tag associati tramite JOIN
     const sql = `
         SELECT *
         FROM movies
@@ -34,24 +34,24 @@ function show(req, res) {
     `;
     connection.query(sql, [id], (err, results) => {
         if (err) {
-            return res.status(500).json({ errore: "Errore nel recupero del post", details: err });
+            return res.status(500).json({ errore: "Errore nel recupero del film", details: err });
         }
         if (results.length === 0) {
             return res.status(404).json({
                 errore: "PostNonTrovato",
                 numero_errore: 404,
-                descrizione: "Nessun post trovato con l'id fornito."
+                descrizione: "Nessun film trovato con l'id fornito."
             });
         }
-        // Ricostruisce il post con i tag
-        const post = {
+        // Ricostruisce il film con i tag
+        const film = {
             id: results[0].id,
             title: results[0].title,
             content: results[0].content,
             path: results[0].path,
             tags: results[0].tag_id ? results.map(r => ({ id: r.tag_id, name: r.label })) : []
         };
-        res.json(post);
+        res.json(film);
     });
 }
 
